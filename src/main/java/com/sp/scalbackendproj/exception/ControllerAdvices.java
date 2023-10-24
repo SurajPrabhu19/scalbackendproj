b30fa34a577634586acf61d26cb1407953f1e6b9
Suraj Prabhu <surajprabhu19@gmail.com>
2023-10-24 16:34:43 +0530

    Adding Controller Advice + Exception Handler

---
+++ src/main/java/com/sp/scalbackendproj/exception/ControllerAdvices.java
@@ -0,0 +1,22 @@
+package com.sp.scalbackendproj.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.sp.scalbackendproj.dto.ExceptionDto;
+
+@ControllerAdvice
+public class ControllerAdvices {
+
+    @ExceptionHandler(NotFoundException.class)
+    private ResponseEntity<ExceptionDto> handleNotFoundException(
+            NotFoundException notFoundException) {
+
+        return new ResponseEntity(
+                new ExceptionDto(HttpStatus.NOT_FOUND, notFoundException.getMessage()),
+                HttpStatus.NOT_FOUND);
+    }
+
+}
